668ecf72cee3abb875819e354bf1a97b66ad2d5d
Floris999 <floris__2@hotmail.com>
2024-07-31 13:48:22 +0200

    Add additional styling

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,11 +1,10 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<!-- <?php flash('post_message'); ?>
-<?php var_dump($_SESSION); ?> -->
-<div class="row mb-3">
+<?php flash('post_message'); ?>
+<div class="d-flex justify-content-between">
     <div class="col-md-6">
         <h1>Posts</h1>
     </div>
-    <div class="col-md-6">
+    <div>
         <a href="<?php echo URLROOT; ?>/posts/add" class="btn btn-primary pull-right">
             <i class="fa fa-pencil"></i>
             Add Post
@@ -17,10 +16,10 @@
         <h4 class="card-title">
             <?php echo $post->title; ?>
         </h4>
-        <div class="bg-light p-2 mb-3">
+        <p class="card-text"><?php echo $post->body; ?></p>
+        <div class="bg-light p-2 mb-3 font-italic">
             written by <?php echo $post->name; ?> on <?php echo $post->postCreated; ?>
         </div>
-        <p class="card-text"><?php echo $post->body; ?></p>
         <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark">More</a>
     </div>
 <?php endforeach; ?>
--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -1,5 +1,5 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<a href="<?php echo URLROOT ?>/posts" class="btn btn-light"><i class="fa fa-pencil"></i>Back</a>
+<a href="<?php echo URLROOT ?>/posts" class="btn btn-primary"><i class="fa fa-pencil"></i>Back</a>
 <h1><?php echo $data['post']->title; ?></h1>
 <div class="bg-secondary text-white p-2 mb-3">
     Written by <?php echo $data['user']->name; ?> on <?php echo $data['post']->created_at; ?>
@@ -8,10 +8,12 @@
 
 <?php if ($data['post']->user_id == $_SESSION['user_id']) : ?>
     <hr>
-    <a href="<?php echo URLROOT; ?>/posts/edit/<?php echo $data['post']->id; ?>" class="btn btn-dark">Edit</a>
+    <div class= 'd-flex justify-content-between'>
+        <a href="<?php echo URLROOT; ?>/posts/edit/<?php echo $data['post']->id; ?>" class="btn btn-dark">Edit</a>
 
-    <form class='pull-right' action='<?php echo URLROOT; ?>/posts/delete/<?php echo $data['post']->id; ?>' method='POST'>
-        <input type="submit" value="Delete" class='btn btn-danger'>
-    </form>
+        <form class='pull-right' action='<?php echo URLROOT; ?>/posts/delete/<?php echo $data['post']->id; ?>' method='POST'>
+            <input type="submit" value="Delete" class='btn btn-danger'>
+        </form>
+    </div>
 <?php endif; ?>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
